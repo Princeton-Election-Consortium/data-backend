e697d5736b2753f4bb7015242660b74595ef7d30
Lucas Manning <lucassm@princeton.edu>
2020-06-17 09:47:22 -0400

    added moving csvs to public access

--- run.sh
+++ run.sh
@@ -4,18 +4,18 @@ source /tmp/update/python/anaconda3/bin/activate
 
 cd /web/data-backend
 
-#python scraping/pec2020.py
-#python banner/banner.py
+python scraping/pec2020.py
+python banner/banner.py
 
 cp /web/data-backend/scraping/outputs/*.txt /web/data-backend/matlab
 cd matlab
 matlab -r "federal_runner; quit"
-matlab -r "Senate_estimator; quit"
 cd ..
 
 cd python_graphics
 python main.py
 
 cp /web/data-backend/matlab/*.txt /web/www/data/election2020
+cp /web/data-backend/scraping/outputs/*.csv /web/www/data/election2020
 cp /web/data-backend/python_graphics/outputs/*.png /web/www/data/election2020
 cp /web/data-backend/banner/banner.html /web/www/data/election2020
