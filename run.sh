f0bfa709a33911ae2a793ee62bbb5d208e2e5cec
Election Admin <election@mol203l.Princeton.EDU>
2020-06-19 12:58:37 -0400

    now archiving pngs

--- run.sh
+++ run.sh
@@ -22,3 +22,8 @@ cp /web/data-backend/matlab/outputs/270towin_URL.txt /web/www/election2020/data
 
 cp /web/data-backend/python_graphics/outputs/*.png /web/www/election2020/outputs
 cp /web/data-backend/banner/banner.html /web/www/election2020/outputs
+
+# backups
+archive_dir=$(date +%Y%m%d_%H%M%S)
+mkdir /web/www/election2020/outputs/archive/$archive_dir
+cp /web/www/election2020/outputs/* /web/www/election2020/outputs/archive/$archive_dir
