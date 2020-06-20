c52ad8860ef5dcf4bff2851fc2efbd2621511391
Mike Hallee <mdhallee@princeton.edu>
2020-06-20 14:59:39 -0400

    changed banner senate href link

--- banner/banner.py
+++ banner/banner.py
@@ -71,7 +71,7 @@ def main():
     <div style="font-weight: 600; width: 970px; color:black ; background-color: #eee ; line-height: 30px; font-family: Helvetica; font-size: 20px">
         <span>{datestring}: Biden {ev_dem} EV ({ev_mm_str}), Senate {sen_seats_dem} D, {sen_seats_rep} R ({sen_mm_str}), House control {gen_mm_str}</span>
         <br>
-        <span>Moneyball states: President NV GA AZ, <a href="https://secure.actblue.com/donate/pec2020federal?refcode=thermometer">Senate</a> MT ME IA, <a href="https://secure.actblue.com/donate/pec2020?refcode=thermometer">Legislatures</a> KS TX NC</span>
+        <span>Moneyball states: President NV GA AZ, <a href="/election-tracking-2020-u-s-senate/">Senate</a> MT ME IA, <a href="https://secure.actblue.com/donate/pec2020?refcode=thermometer">Legislatures</a> KS TX NC</span>
     </div>
     """
 
