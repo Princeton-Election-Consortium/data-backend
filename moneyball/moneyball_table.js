4cd21dff74775b3cbc0afce5261940aa1faea6cb
Mike Hallee <mdhallee@princeton.edu>
2020-07-15 23:49:05 -0400

    base moneyball files

---
+++ moneyball/moneyball_table.js
@@ -0,0 +1,40 @@
+var tabulate = function (data,columns) {
+    var table = d3.select('body').append('table')
+      var thead = table.append('thead')
+      var tbody = table.append('tbody')
+  
+      thead.append('tr')
+        .selectAll('th')
+          .data(columns)
+          .enter()
+        .append('th')
+          .text(function (d) { return d })
+
+                      
+  
+      var rows = tbody.selectAll('tr')
+          .data(data)
+          .enter()
+        .append('tr')
+  
+      var cells = rows.selectAll('td')
+          .data(function(row) {
+              return columns.map(function (column) {
+                  return { column: column, value: row[column] }
+            })
+        })
+        .enter()
+      .append('td')
+        .text(function (d) { return d.value })
+  
+    return table;
+  }
+  
+  d3.csv('TX_voter_power_estimates.csv',function (data) {
+        var columns = ['state', 'district', 'MARGIN', 'VOTER_POWER','NORMALIZED']
+    tabulate(data,columns)
+    console.log(data[0]);
+  })
+
+
+  
